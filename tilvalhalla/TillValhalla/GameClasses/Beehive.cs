f6ac8ed8a86cf12acb8076844f0aa0fd26329199
Kevin Wilson <kevinw@interlink.com>
2022-03-02 20:14:31 -0500

    Updating Project Naming

---
+++ tilvalhalla/TillValhalla/GameClasses/Beehive.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Jotunn;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.GUI;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HarmonyLib;
+using UnityEngine.UI;
+using Logger = Jotunn.Logger;
+
+namespace TillValhalla.GameClasses
+{
+
+    [HarmonyPatch(typeof(Beehive), nameof(Beehive.Awake))]
+    public class BeehivePatch
+    {
+       
+        public static void Postfix(Beehive __instance)
+        {
+            __instance.m_maxHoney = 15;
+
+        }
+    }
+
+    
+
+
+}
---
+++ tilvalhalla/TillValhalla/GameClasses/ItemDrop.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+namespace TillValhalla.GameClasses
+{
+
+    [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.Awake))]
+    public static class noItemTeleportPrevention
+    {
+        public static void Postfix(ItemDrop __instance)
+        {
+            __instance.m_itemData.m_shared.m_teleportable = true;
+
+        }
+    }
+}
