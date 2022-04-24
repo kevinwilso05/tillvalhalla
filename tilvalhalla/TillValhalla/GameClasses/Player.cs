9d9a2bd8e5ec63cc887636bf415e5ac64913063a
Kevin Wilson <kevinw@interlink.com>
2022-04-24 14:41:47 -0400

    Commit after merge

--- tilvalhalla/TillValhalla/GameClasses/Player.cs
+++ tilvalhalla/TillValhalla/GameClasses/Player.cs
@@ -51,30 +51,6 @@ namespace TillValhalla.GameClasses
         }
     }
 
-    [HarmonyPatch(typeof(Game), nameof(Game.UpdateRespawn))]
-    public static class Player_UpdateRespawn_Transpiler
-    {
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            //if (!Configuration.Current.Game.IsEnabled) return instructions;
-
-            string firstarrival = "I have cum";
-
-            List<CodeInstruction> il = instructions.ToList();
-            for (int i = 0; i < il.Count; i++)
-            {
-                if (il[i].opcode == OpCodes.Ldstr)
-                {
-                    il[i].operand = firstarrival;
-                    return il.AsEnumerable();
-                }
-            }
-
-            //ZLog.LogError("Failed to apply Game_GetPlayerDifficulty_Patch.Transpiler");
-
-            return instructions;
-        }
-    }
+    
 }
 
