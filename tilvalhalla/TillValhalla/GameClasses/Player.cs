cd75bf33de67fd9098c1d8cca57826b665f497a5
Kevin Wilson <kevinw@interlink.com>
2022-04-24 14:32:23 -0400

    Added new on first spawn message.

--- tilvalhalla/TillValhalla/GameClasses/Player.cs
+++ tilvalhalla/TillValhalla/GameClasses/Player.cs
@@ -1,5 +1,10 @@
 ﻿using HarmonyLib;
 using TillValhalla.Configurations.Sections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System;
 
 
 namespace TillValhalla.GameClasses
@@ -46,6 +51,30 @@ namespace TillValhalla.GameClasses
         }
     }
 
+    [HarmonyPatch(typeof(Game), nameof(Game.UpdateRespawn))]
+    public static class Player_UpdateRespawn_Transpiler
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            //if (!Configuration.Current.Game.IsEnabled) return instructions;
 
+            string firstarrival = "I have cum";
+
+            List<CodeInstruction> il = instructions.ToList();
+            for (int i = 0; i < il.Count; i++)
+            {
+                if (il[i].opcode == OpCodes.Ldstr)
+                {
+                    il[i].operand = firstarrival;
+                    return il.AsEnumerable();
+                }
+            }
+
+            //ZLog.LogError("Failed to apply Game_GetPlayerDifficulty_Patch.Transpiler");
+
+            return instructions;
+        }
+    }
 }
 
