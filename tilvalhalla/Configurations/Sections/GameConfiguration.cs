1f84d4ad5109c5038f98f3f00020373eac751a7e
Kevin Wilson <kevinw@interlink.com>
2022-04-30 20:09:02 -0400

    Removed server side setting for first spawn from game configuration.

--- tilvalhalla/Configurations/Sections/GameConfiguration.cs
+++ tilvalhalla/Configurations/Sections/GameConfiguration.cs
@@ -12,7 +12,7 @@ namespace TillValhalla.Configurations.Sections
         public static void Awake(BaseUnityPlugin gamecfg)
         {
             enabled = gamecfg.Config.Bind("Game", "enabled", false, new ConfigDescription("Set this to true if you want game configuration section enabled", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
-            firstspawnmessage = gamecfg.Config.Bind("Game", "firstspawnmessage", "I have arrived" , new ConfigDescription("This is the message that displays when a player has arrived", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            firstspawnmessage = gamecfg.Config.Bind("Game", "firstspawnmessage", "I have arrived at an orgasm!" , new ConfigDescription("This is the message that displays when a player has arrived", null));
            
 
         }
