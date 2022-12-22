5cfe546cbdbf9cce9b319ebb343dad09cd499533
Kevin Wilson <kevinw@interlink.com>
2022-12-22 13:26:08 -0500

    Adding in ship and cart crafting configuration sections.

--- tilvalhalla/Configurations/Sections/ContainerConfiguration.cs
+++ tilvalhalla/Configurations/Sections/ContainerConfiguration.cs
@@ -14,6 +14,12 @@ namespace TillValhalla.Configurations.Sections
         public static ConfigEntry<int> blackMetalChestCol; 
         public static ConfigEntry<int> privateChestRows;
         public static ConfigEntry<int> privateChestCol;
+        public static ConfigEntry<int> karveChestRows;
+        public static ConfigEntry<int> karveChestCol;
+        public static ConfigEntry<int> vikingshipChestRows;
+        public static ConfigEntry<int> vikingshipChestCol;
+        public static ConfigEntry<int> cartChestRows;
+        public static ConfigEntry<int> cartChestCol;
 
         public static void Awake(BaseUnityPlugin containercfg)
         {
@@ -32,6 +38,15 @@ namespace TillValhalla.Configurations.Sections
             
             privateChestRows = containercfg.Config.Bind("Container", "Private Chest Rows", 4, new ConfigDescription("This setting affects the rows in private chests", new AcceptableValueRange<int>(3, 8), new ConfigurationManagerAttributes { IsAdminOnly = true }));
             privateChestCol = containercfg.Config.Bind("Container", "Private Chest Columns", 6, new ConfigDescription("This setting affects the columns in private chests", new AcceptableValueRange<int>(2, 20), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+
+            karveChestRows = containercfg.Config.Bind("Container", "Karve Chest Rows", 2, new ConfigDescription("This setting affects the rows in private chests", new AcceptableValueRange<int>(2, 8), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            karveChestCol = containercfg.Config.Bind("Container", "Karve Chest Columns", 2, new ConfigDescription("This setting affects the columns in private chests", new AcceptableValueRange<int>(2, 30), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+
+            vikingshipChestRows = containercfg.Config.Bind("Container", "Viking Ship Chest Rows", 6, new ConfigDescription("This setting affects the rows in private chests", new AcceptableValueRange<int>(6, 8), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            vikingshipChestCol = containercfg.Config.Bind("Container", "Viking Ship Columns", 3, new ConfigDescription("This setting affects the columns in private chests", new AcceptableValueRange<int>(3, 30), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+
+            cartChestRows = containercfg.Config.Bind("Container", "Cart Chest Rows", 6, new ConfigDescription("This setting affects the rows in private chests", new AcceptableValueRange<int>(6, 8), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            cartChestCol = containercfg.Config.Bind("Container", "Cart Chest Columns", 4, new ConfigDescription("This setting affects the columns in private chests", new AcceptableValueRange<int>(3, 30), new ConfigurationManagerAttributes { IsAdminOnly = true }));
 
         }
     }
