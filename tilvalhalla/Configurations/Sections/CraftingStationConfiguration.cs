46ff03693ffba7af055aba25228d38e2b7a29916
Kevin Wilson <kevinw@interlink.com>
2022-12-23 10:28:19 -0500

    Adding in configuration items.

--- tilvalhalla/Configurations/Sections/CraftingStationConfiguration.cs
+++ tilvalhalla/Configurations/Sections/CraftingStationConfiguration.cs
@@ -7,26 +7,30 @@ namespace TillValhalla.Configurations.Sections
 {
     public class CraftingStationConfiguration
     {
+        public static ConfigEntry<bool> enabled; 
         public static ConfigEntry<bool> craftingroofrequired;
         public static ConfigEntry<bool> craftFromCarts;
         public static ConfigEntry<bool> craftFromShips;
         public static ConfigEntry<bool> craftFromChests;
         public static ConfigEntry<bool> craftFromWorkbench;
         public static ConfigEntry<bool> ignorePrivateAreaCheck;
-        public static ConfigEntry<float> craftFromChestRange;
-
+        public static ConfigEntry<int> craftFromChestRange;
+        public static ConfigEntry<bool> arearepairenabled;
+        public static ConfigEntry<int> arearepairrange; 
 
         public static void Awake(BaseUnityPlugin craftingcfg)
         {
 
+            enabled = craftingcfg.Config.Bind("Crafting", "Crafting Section Enabled", true, new ConfigDescription("Set this to false to disable the crafting section.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             craftingroofrequired = craftingcfg.Config.Bind("Crafting", "craftingroofrequired", true, new ConfigDescription("Set this to false to disable crafting stations needing a roof.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             craftFromCarts = craftingcfg.Config.Bind("Crafting", "Craft from Carts", false, new ConfigDescription("Set this to true to enable crafting from carts.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             craftFromShips = craftingcfg.Config.Bind("Crafting", "Craft from Ships", false, new ConfigDescription("Set this to true to enable crafting from ships.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             craftFromChests = craftingcfg.Config.Bind("Crafting", "Craft from Chests", false, new ConfigDescription("Set this to true to enable crafting from Chests.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             craftFromWorkbench = craftingcfg.Config.Bind("Crafting", "Craft from Workbench", false, new ConfigDescription("If in a workbench area, use it as a reference point when scanning for chests.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             ignorePrivateAreaCheck = craftingcfg.Config.Bind("Crafting", "Ignore Private Area Check", false, new ConfigDescription("Set this to true to allow crafting from warded areas", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
-            craftFromChestRange = craftingcfg.Config.Bind("Crafting", "Craft from Chest Range", 50f, new ConfigDescription("Range of the detection of chests.", new AcceptableValueRange<float>(1f, 50f), new ConfigurationManagerAttributes { IsAdminOnly = true }));
-
+            craftFromChestRange = craftingcfg.Config.Bind("Crafting", "Craft from Chest Range", 50, new ConfigDescription("Range of the detection of chests.", new AcceptableValueRange<int>(1, 50), new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            arearepairenabled = craftingcfg.Config.Bind("Crafting", "Enable Area Repair", false, new ConfigDescription("Set this to true to enable area repair.", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            arearepairrange = craftingcfg.Config.Bind("Crafting", "Area Repair Range", 20, new ConfigDescription("Radius of the area repair", new AcceptableValueRange<int>(1, 50), new ConfigurationManagerAttributes { IsAdminOnly = true }));
         }
     }
 }
--- tilvalhalla/Configurations/Sections/PlayerConfiguration.cs
+++ tilvalhalla/Configurations/Sections/PlayerConfiguration.cs
@@ -11,6 +11,7 @@ namespace TillValhalla.Configurations.Sections
         public static ConfigEntry<float> basestamina;
         public static ConfigEntry<float> basehp;
         public static ConfigEntry<float> baseMegingjordBuff;
+        public static ConfigEntry<bool> autorepair;
         
 
         public static void Awake(BaseUnityPlugin playercfg)
@@ -21,6 +22,7 @@ namespace TillValhalla.Configurations.Sections
             basestamina = playercfg.Config.Bind("Player", "basestamina", 75f, new ConfigDescription("Base amount of stamina", null , new ConfigurationManagerAttributes { IsAdminOnly = true }));
             basehp = playercfg.Config.Bind("Player", "basehp", 25f, new ConfigDescription("Base amount of HP", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             baseMegingjordBuff = playercfg.Config.Bind("Player", "baseMegingjordBuff", 200f, new ConfigDescription("Base Megingjord Buff", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            autorepair = playercfg.Config.Bind("Player", "auto repair", false, new ConfigDescription("Set this to true if you want to auto repair items when interacting with workbench", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
         }
 
     }
