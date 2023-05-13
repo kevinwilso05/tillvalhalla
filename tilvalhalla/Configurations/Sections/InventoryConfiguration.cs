3cf683f997ae1257e23df30a0e040b443f0b9124
kevinw@interlink.com <kevinw@interlink.com>
2023-05-13 19:47:51 -0400

    Added ability to modify max stack size. Closes #29

--- tilvalhalla/Configurations/Sections/InventoryConfiguration.cs
+++ tilvalhalla/Configurations/Sections/InventoryConfiguration.cs
@@ -8,7 +8,8 @@ namespace TillValhalla.Configurations.Sections
         public static ConfigEntry<bool> enabled;
         public static ConfigEntry<int> playerinventoryrows;
         public static ConfigEntry<bool> toptobottomfill;
-        
+        public static ConfigEntry<float> maxstacksizemultiplier;
+
 
         public static void Awake(BaseUnityPlugin inventorycfg)
         {
@@ -16,6 +17,8 @@ namespace TillValhalla.Configurations.Sections
             enabled = inventorycfg.Config.Bind("Inventory", "enabled", false, new ConfigDescription("Set this to true to enable the inventory section", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
             playerinventoryrows = inventorycfg.Config.Bind("Inventory", "playerinventoryrows", 4, new ConfigDescription("Determines the amount of inventory rows", new AcceptableValueRange<int>(4,20), new ConfigurationManagerAttributes { IsAdminOnly = true }));
             toptobottomfill = inventorycfg.Config.Bind("Inventory", "toptobottomfill", true , new ConfigDescription("Set this to true to enable the inventory to fill from the top to the bototm",null , new ConfigurationManagerAttributes { IsAdminOnly = true }));
+            maxstacksizemultiplier = inventorycfg.Config.Bind("Inventory", "maxstacksizemultiplier", 0f, new ConfigDescription("Determines the stack size of items in a % multiplier", null, new ConfigurationManagerAttributes { IsAdminOnly = true }));
+
         }
     }
 }
--- tilvalhalla/GameClasses/ItemDrop.cs
+++ tilvalhalla/GameClasses/ItemDrop.cs
@@ -12,7 +12,7 @@ namespace TillValhalla.GameClasses
     [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.Awake))]
     public static class ItemDrop_Awake_Patch
     {
-        
+
 
         public static void Postfix(ItemDrop __instance)
         {
@@ -21,21 +21,29 @@ namespace TillValhalla.GameClasses
                 __instance.m_itemData.m_shared.m_teleportable = true;
             }
 
-        if (Configuration.modisenabled.Value) //Check if Mod is Enabled
-        {
-            //Check type on item and set movement modifier on equip to 0
-        var itemtype = __instance.m_itemData.m_shared.m_itemType.ToString();
-
-
-           //All items movement speed modifier
+            if (Configuration.modisenabled.Value) //Check if Mod is Enabled
+            {
+                //Check type on item and set movement modifier on equip to 0
+                var itemtype = __instance.m_itemData.m_shared.m_itemType.ToString();
+                int maxstack = __instance.m_itemData.m_shared.m_maxStackSize;
+                float maxstackmodified = helper.applyModifierValue(maxstack, inventoryconfiguration.maxstacksizemultiplier.Value);
+                itemtype = __instance.m_itemData.m_shared.m_itemType.ToString(); 
+                if (itemtype == "Consumable" || itemtype == "Material" || itemtype == "Ammo")
+                {
+                    __instance.m_itemData.m_shared.m_maxStackSize = (int)Math.Round(maxstackmodified);
+                }
+                //All items movement speed modifier
                 __instance.m_itemData.m_shared.m_movementModifier = ItemDropConfiguration.movementmodifier.Value;
                 
-            
+
+
+
+
         }
 
-            
 
-    }
+
+        }
     }
     
     
