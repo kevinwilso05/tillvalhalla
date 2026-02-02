0a534aa7be7fd588b7249463a5ddd3970e259011
Kevin Wilson <kevinw@interlink.com>
2026-02-02 17:40:57 -0500

    Adding change log and updating version.

--- tilvalhalla/TillValhalla.cs
+++ tilvalhalla/TillValhalla.cs
@@ -34,7 +34,7 @@ namespace TillValhalla
     {
         public const string PluginGUID = "kwilson.TillValhalla";
         public const string PluginName = "TillValhalla";
-        public const string PluginVersion = "2.4.16";
+        public const string PluginVersion = "2.4.17";
 
         public readonly Harmony _harmony = new Harmony(PluginGUID);
 
